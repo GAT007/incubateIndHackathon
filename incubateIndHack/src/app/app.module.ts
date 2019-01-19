4644665e2cd9e634fe96dc951cde8292dc5508e0
rrajpurohit <rrajpurohit@manh.com>
2019-01-19 14:09:13 +0530

    DOM-56199 :SOX Order Audit UI : hidden Buttons condition

--- incubateIndHack/src/app/app.module.ts
+++ incubateIndHack/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ContactDetailsComponent } from './userModule/contact-details/contact-details.component';
+import { RouterModule } from '@angular/router';
+import { APP_ROUTES } from './general-component/app-router/app-router.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      APP_ROUTES,
+      {
+        enableTracing: true,
+        useHash: true
+      }// <-- debugging purposes only
+    ),
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ incubateIndHack/src/app/general-component/app-router/app-router.module.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+import { ContactDetailsComponent } from '../../userModule/contact-details/contact-details.component';
+
+
+export const APP_ROUTES: Routes = [
+  {
+    path: "",
+    component: ContactDetailsComponent
+  }
+];
+
+
+
+
