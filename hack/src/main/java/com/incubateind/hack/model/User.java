9ba2a107a89d53dcde371886a34f1387d78a8688
atallanki <atallanki@manh.com>
2019-01-20 03:11:07 +0530

    Adding Models and Repository Classes for Users, Interests and skills

---
+++ hack/src/main/java/com/incubateind/hack/model/User.java
@@ -0,0 +1,82 @@
+package com.incubateind.hack.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "user")
+public class User extends AuditModel
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(max=300)
+	private String username;
+	
+	@NotNull
+	private String password;
+	
+	@NotNull
+	private String college;
+	
+	@NotNull
+	private Integer experiencePoints;
+	
+	private boolean isAdmin;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCollege() {
+		return college;
+	}
+
+	public void setCollege(String college) {
+		this.college = college;
+	}
+
+	public Integer getExperiencePoints() {
+		return experiencePoints;
+	}
+
+	public void setExperiencePoints(Integer experiencePoints) {
+		this.experiencePoints = experiencePoints;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	
+}
